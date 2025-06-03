9af2ce1a0a77ca68f0db8bce95c372c03297ec70
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-03 22:48:28 +0530

    Even Index Value

--- Arrays/OddIndexValuePrint.java
+++ Arrays/OddIndexValuePrint.java
@@ -1,10 +1,10 @@
 public class OddIndexValuePrint {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6,7,8,9,0};
+        int arr[]={1,2,3,4,5,6,7,8,9,0,2,4};
         for(int i=0;i<arr.length;i++){
             if(i%2!=0){
                 System.out.print(arr[i]);
-                if(i != arr.length - 1)
+                if(i != arr.length - 1 && i != arr.length-2)
                     System.out.print(",");
             }
         }
