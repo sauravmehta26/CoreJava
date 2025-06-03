85909e3d9a91c89e9700d6157d268e92f9533557
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-03 22:26:12 +0530

    Even Index Value

---
+++ Arrays/EvenIndexValue.java
@@ -0,0 +1,11 @@
+public class EvenIndexValue {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,0};
+        for(int i=0;i<arr.length;i++){
+            if(i%2==0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+    
+}
