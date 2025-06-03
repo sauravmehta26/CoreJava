c8087044c34e3b011b344f748daf1d41afc1c361
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-03 22:35:11 +0530

    Odd Index Value Print

---
+++ Arrays/OddIndexValuePrint.java
@@ -0,0 +1,13 @@
+public class OddIndexValuePrint {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,0};
+        for(int i=0;i<arr.length;i++){
+            if(i%2!=0){
+                System.out.print(arr[i]);
+                if(i != arr.length - 1)
+                    System.out.print(",");
+            }
+        }
+    }
+    
+}
