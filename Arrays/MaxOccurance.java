76db140a8d336616042dbe264b0819423b1a75f8
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-06 00:53:22 +0530

    Max Occurance ele

--- Arrays/MaxOccurance.java
+++ Arrays/MaxOccurance.java
@@ -1,19 +1,17 @@
 public class MaxOccurance {
 public static void main(String[] args) {
     int arr[]={1,2,1,3,1,4};
-    int maxc=0;
-    int ele=-1;
+    int b[]=new int[10];
     for (int i=0;i<arr.length;i++){
-        int currc=1;
-    for (int j=i+1;j<arr.length-1;j++){
-        if(arr[i]==arr[j]){
-            currc++;
+        b[arr[i]]++;     
+    }  
+    int max=Integer.MIN_VALUE;
+    int ele=0;
+    for(int i=0;i<b.length;i++){
+        if(b[i]>max){
+            max=b[i];
+            ele=i;
         }
-    }
-    if(maxc<currc){
-        maxc=currc;
-        ele=arr[i];
-    }
-}System.out.println(ele+" occured "+maxc+" times.");
-}    
+    } System.out.println(max+" times "+ele); 
+}
 }
