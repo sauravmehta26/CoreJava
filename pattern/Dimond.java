1924177f329483b47e372118d48833f9618d8941
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-17 11:28:58 +0530

    dimond

--- pattern/Dimond.java
+++ pattern/Dimond.java
@@ -1,3 +1,31 @@
 public class Dimond {
-  
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+public static void main(String[] args) {
+    int n = 7;
+    int space =n/2 ;
+    int stars = 1;
+    for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+            if (i <= n / 2) {
+                space--;
+                stars += 2;
+            } else {
+                space++;
+                stars -= 2;
+            }
+        }
+}  
 }
--- pattern/Rombus.java
+++ pattern/Rombus.java
@@ -1,24 +1,26 @@
 public class Rombus {
     public static void main(String[] args) {
         int n=7;
-        for(int i=1;i<=n/2+1;i++){
-            for(int s=n-n/2-i; s>=1;s--){
-            System.out.print(" ");
+        int spaces = n/2;
+        int stars = 1;
+        for(int i=1;i<=n;i++){
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
             }
-            for(int j=1;j<=2*i-1; j++){
+            for(int j = 1; j <= stars; j++){
                 System.out.print("*");
             }
-            System.out.println();
-            }
-            for(int i=1;i<=n/2;i++){
-                for(int s=1; s<=i;s++){
-                    System.out.print(" ");
-            }
-                for( int j =n-2*i; j >= 1;j--){
-                    System.out.print("*");
-                }
                 System.out.println();
+                if(i <= n/2){
+                    spaces--;
+                    stars += 2;
+                }else{
+                    stars -= 2;
+                    spaces++;
+                }
+
+                }
             }
         }
-        }
+        
     
