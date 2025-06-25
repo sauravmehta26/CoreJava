f59173032fe578c598b2984dfd558eea94416a66
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-25 22:59:19 +0530

    Emirps number

--- recursion/Emirps.java
+++ recursion/Emirps.java
@@ -2,16 +2,30 @@ import java.util.Scanner;
 
 public class Emirps {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number to be checked");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to be checked:");
         int n = sc.nextInt();
-        int o = n;
-        int rev= 0;
-        System.out.println(emirps(n,rev,o));
+
+        if (isEmirp(n)) {
+            System.out.println(n + " is an emirp number.");
+        } else {
+            System.out.println(n + " is NOT an emirp number.");
+        }
     }
-    public static boolean emirps(int n,int rev,int o) {
-        if(n==0) return rev==o;
-        rev = (rev*10)+n%10;
-        return emirps(n/10,rev,o);
+        public static boolean isEmirp(int n) {
+        if (!isPrime(n, n / 2)) return false;
+
+        int rev = reverse(n,0);
+        return rev != n && isPrime(rev, rev / 2);
+    }
+    public static boolean isPrime(int n,int half){
+        if(half==1) return true;
+        if(n<=1 || n%half == 0) return false;
+        return isPrime(n,half-1);
+    }
+    public static int reverse(int n,int rev){
+        if(n == 0) return rev;
+        rev=(rev*10)+(n%10);
+        return reverse(n/2,rev);
     }
 }
