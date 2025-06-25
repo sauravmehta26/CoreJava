aae16fe6293dd3366eed4d50f327c6c9b72351bb
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-25 19:19:13 +0530

    armstrong number

--- recursion/ArmStrong.java
+++ recursion/ArmStrong.java
@@ -5,14 +5,32 @@ public class ArmStrong {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the number to be checked");
         int n = sc.nextInt();
+        int m = n;
         int sum= 0;
-        System.out.println(armstrong(n,sqr,sum));
+        int length=len(n);
+        System.out.println(armstrong(n,length,sum,m));
 
     }
-       public static boolean armstrong(int n,int sqr,int sum){
-            if(sqr == 0)return sum==n;
-            sum+=sqr%10;
-            return armstrong(n,sqr/10,sum);
+    public static int len(int n){
+        int l=0;
+        while(n>0){
+            n=n/10;
+        l++;
+        }
+        return l;
+    }
+        public static int power(int base, int exp) {
+        int result = 1;
+        for (int i = 0; i < exp; i++) {
+            result *= base;
+        }
+        return result;
+    }
+       public static boolean armstrong(int n,int len,int sum,int m){
+            if(n == 0)return sum==m;
+            int r=n%10;
+            sum+=power(r,len);
+            return armstrong(n/10,len,sum,m);
 
         } 
 }
