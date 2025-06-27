c4f985a629c250b3309c0cf8cd1e7f96533aa784
sauravmehta26 <sauravmehta786@gmail.com>
2025-06-27 10:28:07 +0530

    rev halo triangle

--- pattern/RevHaloT.java
+++ pattern/RevHaloT.java
@@ -1,7 +1,7 @@
 public class RevHaloT {
     public static void main(String[] args) {
         int n = 5;
-        int sp = 0;
+        int sp = 1;
         int st = 2*n-1;
         for(int i = 1;i <= n;i++){
             for(int s = 1;s <= sp; s++){
