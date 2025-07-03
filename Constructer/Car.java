0aafdc2fc8c8fc032ea73f69a3b8d13379f86206
sauravmehta26 <sauravmehta786@gmail.com>
2025-07-04 00:43:32 +0530

    ERROR

---
+++ Constructer/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    static int i;
+    
+    i=1;//wrong
+    
+    int j=0;//correct
+    int k;
+    k=7;//wrong
+    
+    public static void main(String[] args) {
+        // Car b=new Car();
+        System.out.println(i);
+    }
+    
+}
