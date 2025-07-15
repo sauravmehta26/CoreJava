a46152fb4879c980b2b5651a6605cae311fe9104
sauravmehta26 <sauravmehta786@gmail.com>
2025-07-15 17:42:18 +0530

    hi

--- pattern/SnaclTriangle.java
+++ pattern/SnaclTriangle.java
@@ -1,3 +1,23 @@
 public class SnaclTriangle{
+    public static void main(String[] args){
+        int n=5;
+        int x=1;
+        for(int i = 1; i <= n; i++){
+            if(i%2!=0){
+            for(int j = 1; j <=i; j++){
+                System.out.print(x++ +" ");
+            }
+            }else{
+                int y=x+i-1;
+                x=y+1;
+                for(int j = 1; j <=i; j++){
+                System.out.print(y-- +" ");
+            }
+            
+            }
+            System.out.println();
+           
+        }
+    }
 
 }
